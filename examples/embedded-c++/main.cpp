213fbe78a69107ee8ff145e807e7c3a627b74faa
kabu1204 <yuchengye2013@outlook.com>
2023-09-15 19:39:45 +0800

    opt: test example

--- examples/embedded-c++/main.cpp
+++ examples/embedded-c++/main.cpp
@@ -1,14 +1,24 @@
 #include "duckdb.hpp"
+#include <string>
+
+// using namespace duckdb;
 
 using namespace duckdb;
 
 int main() {
-	DuckDB db(nullptr);
-
+	DuckDB db("/home/ycy/repo/duckdb/run/test.db");
 	Connection con(db);
+	std::string value;
+	unique_ptr<MaterializedQueryResult> result;
 
-	con.Query("CREATE TABLE integers(i INTEGER)");
-	con.Query("INSERT INTO integers VALUES (3)");
-	auto result = con.Query("SELECT * FROM integers");
-	result->Print();
+	result = con.Query("CREATE TABLE test_tb(s VARCHAR)");
+
+	for(int i=0; i< 1000; i++) {
+		value.assign("value"+std::to_string(i));
+		con.Query("INSERT INTO test_tb VALUES ('" + value + "')");
+	}
+	
+	result = con.Query("SELECT * FROM test_tb");
+	value.assign(result->ToString());
+	return 0;
 }
